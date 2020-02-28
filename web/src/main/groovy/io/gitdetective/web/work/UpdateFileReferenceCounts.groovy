a5ab9ab6016f2b446445e721920c27e53e8b081e
Brandon Fergerson <fergerson92@gmail.com>
2020-02-28 07:20:50 -0500

    logging + periodic + timing + on boot

--- web/src/main/groovy/io/gitdetective/web/work/UpdateFileReferenceCounts.groovy
+++ web/src/main/groovy/io/gitdetective/web/work/UpdateFileReferenceCounts.groovy
@@ -1,13 +1,18 @@
 package io.gitdetective.web.work
 
 import grakn.client.GraknClient
+import groovy.util.logging.Slf4j
+import io.gitdetective.web.WebLauncher
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.AsyncResult
 import io.vertx.core.Future
 import io.vertx.core.Handler
 
+import java.util.concurrent.TimeUnit
+
 import static graql.lang.Graql.*
 
+@Slf4j
 class UpdateFileReferenceCounts extends AbstractVerticle {
 
     public static final String PERFORM_TASK_NOW = "UpdateFileReferenceCounts"
@@ -20,16 +25,28 @@ class UpdateFileReferenceCounts extends AbstractVerticle {
 
     @Override
     void start() throws Exception {
-        //todo: once a day
+        def timer = WebLauncher.metrics.timer(PERFORM_TASK_NOW)
         vertx.eventBus().consumer(PERFORM_TASK_NOW, request -> {
+            def time = timer.time()
+            log.info(PERFORM_TASK_NOW + " started")
+
             updateGraknFileReferenceCounts({
                 if (it.succeeded()) {
                     request.reply(true)
                 } else {
                     request.fail(500, it.cause().message)
                 }
+
+                log.info(PERFORM_TASK_NOW + " finished")
+                time.close()
             })
         })
+
+        //perform every 45 minutes
+        vertx.setPeriodic(TimeUnit.MINUTES.toMillis(45), {
+            vertx.eventBus().send(PERFORM_TASK_NOW, true)
+        })
+        vertx.eventBus().send(PERFORM_TASK_NOW, true) //perform on boot
     }
 
     void updateGraknFileReferenceCounts(Handler<AsyncResult<Void>> handler) {
--- web/src/main/groovy/io/gitdetective/web/work/UpdateFunctionReferenceCounts.groovy
+++ web/src/main/groovy/io/gitdetective/web/work/UpdateFunctionReferenceCounts.groovy
@@ -1,6 +1,8 @@
 package io.gitdetective.web.work
 
 import grakn.client.GraknClient
+import groovy.util.logging.Slf4j
+import io.gitdetective.web.WebLauncher
 import io.gitdetective.web.dao.PostgresDAO
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.AsyncResult
@@ -12,10 +14,12 @@ import io.vertx.sqlclient.RowStream
 import io.vertx.sqlclient.Transaction
 import io.vertx.sqlclient.impl.ArrayTuple
 
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicReference
 
 import static graql.lang.Graql.*
 
+@Slf4j
 class UpdateFunctionReferenceCounts extends AbstractVerticle {
 
     public static final String PERFORM_TASK_NOW = "UpdateFunctionReferenceCounts"
@@ -37,16 +41,28 @@ class UpdateFunctionReferenceCounts extends AbstractVerticle {
 
     @Override
     void start() throws Exception {
-        //todo: once a day
+        def timer = WebLauncher.metrics.timer(PERFORM_TASK_NOW)
         vertx.eventBus().consumer(PERFORM_TASK_NOW, request -> {
+            def time = timer.time()
+            log.info(PERFORM_TASK_NOW + " started")
+
             createFunctionReferenceCountTable({
                 if (it.succeeded()) {
                     request.reply(true)
                 } else {
                     request.fail(500, it.cause().message)
                 }
+
+                log.info(PERFORM_TASK_NOW + " finished")
+                time.close()
             })
         })
+
+        //perform every 30 minutes
+        vertx.setPeriodic(TimeUnit.MINUTES.toMillis(30), {
+            vertx.eventBus().send(PERFORM_TASK_NOW, true)
+        })
+        vertx.eventBus().send(PERFORM_TASK_NOW, true) //perform on boot
     }
 
     void createFunctionReferenceCountTable(Handler<AsyncResult<Void>> handler) {
